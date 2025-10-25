92b6bb2bc01ae654da29fc56d252143d9a873cfa
Spencer <salane2232@gmail.com>
2025-10-25 13:58:20 -0400

    Task 3 Commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,21 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-
+        //array declaration
         int[] dataPoints = new int[100];
+
+        Random rnd = new Random();
+            for (int i = 0; i < dataPoints.length; i++)
+            {
+                dataPoints[i] = rnd.nextInt(100) + 1;
+            }
+            for (int i = 0; i < dataPoints.length; i++)
+            {
+                System.out.print(dataPoints[i]);
+                if (i < dataPoints.length - 1) ;
+                {
+                    System.out.print(" | ");
+                }
+            }
     }
 }
