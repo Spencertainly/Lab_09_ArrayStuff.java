fc852cde30db127fa0cd47a2de64f496861e53e1
Spencer <salane2232@gmail.com>
2025-10-25 14:37:01 -0400

    Task 6 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args)
@@ -28,5 +29,21 @@ public class Main {
         // sum and average output
         System.out.println("The sum of the random array dataPoints is: " + sum);
         System.out.println("The average of the random array dataPoints is: " + average);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print ("Enter a value between 1 and 100 to search for in the dataPoints array: ");
+        int userValue = scanner.nextInt();
+
+        int count = 0;
+        for (int i = 0; i < dataPoints.length; i++)
+        {
+            if (dataPoints[i] == userValue)
+            {
+                count++;
+            }
+        }
+        //Results
+        System.out.println("Your value " + userValue + " was found " + count + " time(s) in the dataPoint array!");
+        scanner.close();
     }
 }
